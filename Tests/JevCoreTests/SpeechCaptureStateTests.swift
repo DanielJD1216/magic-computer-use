8ac369f4f68a904a1c08c6dd9eb46510f1331260
DanielJD1216 <104115703+DanielJD1216@users.noreply.github.com>
2026-09-18 19:41:02 -0700

    test: cover repeated speech capture lifecycle

--- Tests/JevCoreTests/SpeechCaptureStateTests.swift
+++ Tests/JevCoreTests/SpeechCaptureStateTests.swift
@@ -15,6 +15,28 @@ final class SpeechCaptureStateTests: XCTestCase {
         XCTAssertEqual(capture.phase, .completed)
     }
 
+    func testTerminalCaptureCanPrepareForNextCapture() {
+        var capture = SpeechCaptureLedger()
+
+        XCTAssertTrue(capture.beginPermissionRequest())
+        XCTAssertTrue(capture.authorize())
+        XCTAssertTrue(capture.releaseCapture())
+        XCTAssertTrue(capture.acceptFinalTranscript())
+
+        XCTAssertTrue(capture.prepareForNextCapture())
+        XCTAssertEqual(capture.phase, .idle)
+        XCTAssertTrue(capture.beginPermissionRequest())
+    }
+
+    func testActiveCaptureCannotBeReplacedByNextCapture() {
+        var capture = SpeechCaptureLedger()
+
+        XCTAssertTrue(capture.beginPermissionRequest())
+        XCTAssertTrue(capture.authorize())
+        XCTAssertFalse(capture.prepareForNextCapture())
+        XCTAssertEqual(capture.phase, .listening)
+    }
+
     func testCancellationRejectsLateFinalTranscript() {
         var capture = SpeechCaptureLedger()
 
